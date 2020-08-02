480862842ec6a5c3c2c33123d37a4d8af699ed15
Damilola Egunjobi <justgunji@gmail.com>
2020-08-03 00:11:50 +0100

    Add Outliers mini project

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -14,6 +14,18 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    error_list = []
+    for ii in range(len(predictions)):
+        error = abs(net_worths[ii] - predictions[ii])
+        error_list.append(error)
+        
+    error_list.sort()
+    reduced_error_list = error_list[:int(len(error_list)*0.9)]
+    
+    for ii in range(len(predictions)):
+        error = abs(net_worths[ii] - predictions[ii])
+        if error in reduced_error_list:
+            cleaned_data.append((ages[ii], net_worths[ii], error))
 
     
     return cleaned_data
